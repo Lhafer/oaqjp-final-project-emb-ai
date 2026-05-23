a34a36625f65a503300ffd92ffcaea0e302cad0c
Logan Shafer <shaferlogancfl@gmail.com>
2026-05-23 16:19:16 -0400

    done

--- server.py
+++ server.py
@@ -1,24 +1,38 @@
+"""
+    Flask app which renders an html
+    frontend which inputs and displays
+    text for the watson emotion predictor
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
-
 
 
 app = Flask("Emotion Detection App")
 
 @app.route("/")
 def home():
+    """
+        renders the app on the base route
+    """
     return render_template("index.html")
 
 @app.route("/emotionDetector")
 def detect():
+    """
+        Takes in text from the request and
+        uses the emotion_detector to analyze
+        it, then returning the text based on
+        the emotions object.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     emotions = emotion_detector(text_to_analyze)
-    
+
     if emotions["dominant_emotion"] is None:
         return "Invalid Text! Please try again!"
 
-    return    f"""For the given statement, the system response is 
-    'anger': {emotions["anger"]}
+    return f"""For the given statement, the system response is
+     'anger': {emotions["anger"]}
     , 'disgust': {emotions["disgust"]}
     , 'fear': {emotions["fear"]}
     , 'sadness': {emotions["sadness"]}
@@ -30,5 +44,3 @@ def detect():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
-
-print(emotion_detector("I'm so happy I could explode"))
